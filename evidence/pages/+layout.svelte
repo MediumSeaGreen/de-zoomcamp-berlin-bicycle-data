b43774449274763d1ab09fbd77e9b667b7560996
MediumSeaGreen <123734057+MediumSeaGreen@users.noreply.github.com>
2025-04-14 17:52:44 +0200

    page optimizations

---
+++ evidence/pages/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '@evidence-dev/tailwind/fonts.css';
+	import '../app.css';
+	import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
+	export let data;
+</script>
+
+<EvidenceDefaultLayout {data} hideSidebar=true title='Fahrradbarometer' neverShowQueries=true githubRepo='https://github.com/MediumSeaGreen/de-zoomcamp-berlin-bicycle-data'>
+	<slot slot="content" />
+</EvidenceDefaultLayout>
